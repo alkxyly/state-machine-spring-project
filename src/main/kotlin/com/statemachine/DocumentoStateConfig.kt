1f3eba82f242cc5adf93f02d669c8e347ef321cc
alkxyly <alkxyly@hotmail.com>
2022-01-23 21:11:42 -0300

    Actions and Guard

--- src/main/kotlin/com/statemachine/DocumentoStateConfig.kt
+++ src/main/kotlin/com/statemachine/DocumentoStateConfig.kt
@@ -2,14 +2,18 @@ package com.statemachine
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.statemachine.StateContext
+import org.springframework.statemachine.action.Action
 import org.springframework.statemachine.config.EnableStateMachine
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer
+import org.springframework.statemachine.guard.Guard
 import org.springframework.statemachine.listener.StateMachineListenerAdapter
 import org.springframework.statemachine.state.State
 import java.util.*
+import javax.swing.text.Document
 
 @Configuration
 @EnableStateMachine
@@ -27,14 +31,18 @@ class DocumentoStateConfig : EnumStateMachineConfigurerAdapter<DocumentoStates, 
        states
             .withStates()
             .initial(DocumentoStates.RASCUNHO)
-            .states(EnumSet.allOf(DocumentoStates::class.java))
+            .state(DocumentoStates.MODERACAO)
+            .state(DocumentoStates.PUBLICADO, enviarEmail(), null)
     }
+
+
 
     override fun configure(transitions: StateMachineTransitionConfigurer<DocumentoStates, DocumentoEvents>) {
         transitions
              .withExternal()
                      .source(DocumentoStates.RASCUNHO).target(DocumentoStates.MODERACAO)
                      .event(DocumentoEvents.PUBLICADO_POR_USUARIO)
+                     .guard(validarGuarda())
             .and()
                 .withExternal()
                     .source(DocumentoStates.RASCUNHO).target(DocumentoStates.PUBLICADO)
@@ -43,12 +51,31 @@ class DocumentoStateConfig : EnumStateMachineConfigurerAdapter<DocumentoStates, 
                 .withExternal()
                     .source(DocumentoStates.MODERACAO).target(DocumentoStates.PUBLICADO)
                     .event(DocumentoEvents.APROVADO)
-
+            .and()
+                .withExternal()
+                     .source(DocumentoStates.MODERACAO).target(DocumentoStates.RASCUNHO)
+                     .event(DocumentoEvents.REVISAO_OU_FALHA)
+            .and()
+                 .withExternal()
+                     .source(DocumentoStates.PUBLICADO).target(DocumentoStates.RASCUNHO)
+                     .event(DocumentoEvents.EXPIROU)
     }
 
     @Bean
     fun getListener() : StateMachineListenerAdapter<DocumentoStates, DocumentoEvents>{
         return StateMachineListenerAdapterImpl()
+    }
+
+    @Bean
+    fun enviarEmail() = EnviarEmailAction()
+
+    @Bean
+    fun validarGuarda() = GuardaParaOEvento()
+}
+
+class EnviarEmailAction: Action<DocumentoStates, DocumentoEvents>{
+    override fun execute(p0: StateContext<DocumentoStates, DocumentoEvents>?) {
+        println("Enviando email quando o documento é publicado ")
     }
 }
 
@@ -59,4 +86,12 @@ class StateMachineListenerAdapterImpl :  StateMachineListenerAdapter<DocumentoSt
         to: State<DocumentoStates, DocumentoEvents>?) {
         println("Estado do documento mudou de "+from?.id +" para "+to?.id)
     }
+}
+
+class GuardaParaOEvento:  Guard<DocumentoStates, DocumentoEvents> {
+    override fun evaluate(p0: StateContext<DocumentoStates, DocumentoEvents>?): Boolean {
+        print(p0?.message?.payload)
+        return true
+    }
+
 }
--- src/main/kotlin/com/statemachine/StateMachineApplication.kt
+++ src/main/kotlin/com/statemachine/StateMachineApplication.kt
@@ -6,7 +6,11 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHeaders
 import org.springframework.statemachine.StateMachine
+import java.util.HashMap
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 
 @SpringBootApplication
@@ -15,10 +19,27 @@ class StateMachineApplication (val stateMachine: StateMachine<DocumentoStates, D
 	val logger: Logger = LoggerFactory.getLogger(StateMachineApplication::class.java)
 
 	override fun run(vararg args: String?) {
-		logger.info("Iniciando Testes")
-		stateMachine.sendEvent(DocumentoEvents.PUBLICADO_POR_USUARIO)
-		stateMachine.sendEvent(DocumentoEvents.APROVADO)
+		logger.info("Iniciando Máquina de Estados")
 
+		with(stateMachine){
+			sendEvent(DocumentoEvents.PUBLICADO_POR_USUARIO)
+			sendEvent(ConfigurarMessage("JOSE"))
+			sendEvent(DocumentoEvents.APROVADO)
+			sendEvent(DocumentoEvents.EXPIROU)
+		}
+
+		logger.info("Fim da Máquina de Estados")
+	}
+}
+
+class ConfigurarMessage (val usuario: String)  : Message<DocumentoEvents>{
+
+	override fun getPayload() = DocumentoEvents.PUBLICADO_POR_USUARIO
+
+	override fun getHeaders(): MessageHeaders {
+		var parametros = HashMap<String, Any>()
+		parametros.put("usuario", this.usuario)
+		return MessageHeaders(parametros);
 	}
 }
 
